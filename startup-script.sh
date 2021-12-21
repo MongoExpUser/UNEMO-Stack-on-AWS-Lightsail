a944c513eb4b2f3ea9ce220739314de5251560dc
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2021-12-20 23:18:48 -0600

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -140,6 +140,7 @@ install_and_configure_nodejs_web_server () {
     sudo npm i pg
     sudo npm i ocsp
     sudo npm i mongodb
+    sudo npm i s3-proxy
     sudo npm i python-shell
     sudo npm i serve-favicon
     sudo npm i serve-index
